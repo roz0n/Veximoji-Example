9e161372bb6b7cebe54182bb7ee7a7e47650a7d0
Arnold Rozon <arnold@rozon.org>
2021-05-21 01:28:39 -0500

    Initial Commit

---
+++ VeximojiDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VeximojiDemo
+//
+//  Created by Arnaldo Rozon on 5/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
